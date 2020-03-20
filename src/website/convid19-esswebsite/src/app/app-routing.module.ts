f32b2d94ab6db7a799679fe7727751ecb1052257
Ndina <nnashandi@nust.na>
2020-03-20 02:53:38 +0200

    new component

--- src/website/convid19-esswebsite/src/app/app-routing.module.ts
+++ src/website/convid19-esswebsite/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import {AboutUsComponent} from './components/about-us/about-us.component';
 
 const routes: Routes = [
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'app-about-us',component: AboutUsComponent},
 ];
 
 @NgModule({
