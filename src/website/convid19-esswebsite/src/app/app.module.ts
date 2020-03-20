e49734f966a437ca050f3030f4353e4de8d6932a
Tinashe Chifamba <cwaziebwoi@gmail.com>
2020-03-20 13:43:55 +0200

    receiving api

--- src/website/convid19-esswebsite/src/app/app.module.ts
+++ src/website/convid19-esswebsite/src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 
+import { GeneralInfoComponent } from './components/general-info/general-info.component';
+
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+
+    GeneralInfoComponent,
+
+    AboutUsComponent
+
   ],
   imports: [
     BrowserModule,
