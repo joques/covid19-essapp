a16e01f83a608bdf6ac7d0d81ca17ad9c46bff57
Tinashe Chifamba <cwaziebwoi@gmail.com>
2020-03-20 03:26:54 +0200

    General Info

--- src/website/convid19-esswebsite/src/app/app-routing.module.ts
+++ src/website/convid19-esswebsite/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { GeneralInfoComponent } from './components/general-info/general-info.component';
 
 const routes: Routes = [
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'general', component: GeneralInfoComponent },
 ];
 
 @NgModule({
--- src/website/convid19-esswebsite/src/app/app.module.ts
+++ src/website/convid19-esswebsite/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { GeneralInfoComponent } from './components/general-info/general-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    GeneralInfoComponent
   ],
   imports: [
     BrowserModule,
