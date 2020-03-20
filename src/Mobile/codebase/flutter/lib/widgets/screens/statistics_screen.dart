69b548931b615be569d8ed9794c5d3b3d588bb33
Daniel Mgawi <pdmgawi@gmail.com>
2020-03-20 16:20:01 +0200

    working on statistics page.

--- src/Mobile/codebase/flutter/lib/widgets/screens/statistics_screen.dart
+++ src/Mobile/codebase/flutter/lib/widgets/screens/statistics_screen.dart
@@ -31,7 +31,40 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text(
-                  "Windhoek",
+                  "Kunene",
+                ),
+              ],
+            ),
+          ),
+          DropdownMenuItem(
+            value: "3",
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Kavango Region",
+                ),
+              ],
+            ),
+          ),
+          DropdownMenuItem(
+            value: "4",
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Khomas",
+                ),
+              ],
+            ),
+          ),
+          DropdownMenuItem(
+            value: "5",
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Erongo",
                 ),
               ],
             ),
@@ -60,27 +93,166 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
       body: Column(
         children: <Widget>[
           Container(
-              height: 45,
-              margin: EdgeInsets.only(
-                left: 20,
-                right: 20,
-                top: 16,
-                bottom: 16,
-              ),
-              child: _itemDown()),
-          Container(
-            height: 45,
+            height: 70,
             margin: EdgeInsets.only(
               left: 20,
               right: 20,
-              top: 86,
-              bottom: 86,
+              top: 15,
+              bottom: 15,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    width: 79,
+                    child: Text(
+                      "Statistics",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: AppColors.primaryText,
+                        fontFamily: "Roboto",
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    margin: EdgeInsets.only(top: 3),
+                    child: Text(
+                      "As of Thur, 19 March 2020, 7:30 am",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: AppColors.secondaryText,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 120,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 10,
+              top: 10,
+              bottom: 10,
+            ),
+            child: Row(
+              children: [
+                Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: AppColors.accentElement,
+                              borderRadius: BorderRadius.circular(9.0),
+                            ),
+                            child: Container()),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 10,
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: AppColors.accentElement,
+                              borderRadius: BorderRadius.circular(9.0),
+                            ),
+                            child: Container()),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 10,
+                  height: 10,
+                ),
+                Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: AppColors.accentElement,
+                              borderRadius: BorderRadius.circular(9.0),
+                            ),
+                            child: Container()),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 10,
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: AppColors.accentElement,
+                              borderRadius: BorderRadius.circular(9.0),
+                            ),
+                            child: Container()),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+              height: 50,
+              margin: EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 15,
+                bottom: 15,
+              ),
+              decoration: BoxDecoration(
+                color: AppColors.accentElement,
+                borderRadius: BorderRadius.circular(9.0),
+              ),
+              child: Container(
+                  margin: EdgeInsets.only(
+                    left: 15,
+                    right: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColors.accentElement,
+                  ),
+                  child: _itemDown())),
+          Container(
+            height: 70,
+            margin: EdgeInsets.only(
+              left: 20,
+              right: 20,
+              top: 15,
+              bottom: 15,
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Container(
-                  height: 25,
+                  height: 40,
                   child: Row(
                     children: [
                       Text(
@@ -88,7 +260,6 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
                         textAlign: TextAlign.left,
                         style: TextStyle(
                           color: AppColors.primaryText,
-                          fontFamily: "Roboto",
                           fontWeight: FontWeight.w700,
                           fontSize: 16,
                         ),
@@ -96,7 +267,7 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
                       Spacer(),
                       Container(
                         width: 52,
-                        height: 32,
+                        height: 45,
                         child: Switch.adaptive(
                           value: true,
                           inactiveTrackColor: Color.fromARGB(60, 0, 0, 0),
@@ -112,11 +283,11 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
                 Container(
                   margin: EdgeInsets.only(right: 66),
                   child: Text(
-                    "By Enabling this option you will receive the latest notifications ",
+                    "By Enabling this option you will receive the latest notifications as they happen.",
                     textAlign: TextAlign.left,
                     style: TextStyle(
                       color: AppColors.secondaryText,
-                      fontSize: 10,
+                      fontSize: 13,
                     ),
                   ),
                 ),
