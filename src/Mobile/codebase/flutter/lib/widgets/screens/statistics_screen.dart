fd88ace1da429be1a238bf1dbb7c2ec8638fe146
Daniel Mgawi <pdmgawi@gmail.com>
2020-03-21 22:28:16 +0200

    1. Added FAQ Tab & Page 2. Added symptoms page, there is no navigation to it yet. 3. Scrolling to prevent overflow, on smaller screen. (Please Test on IPhone Device)

--- src/Mobile/codebase/flutter/lib/widgets/screens/statistics_screen.dart
+++ src/Mobile/codebase/flutter/lib/widgets/screens/statistics_screen.dart
@@ -20,7 +20,8 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
   //View By Area Drop Down Value
   var _value;
 
-  DropdownButton _itemDown() => DropdownButton<String>(
+  //Location Name List
+  DropdownButton _locationData() => DropdownButton<String>(
         items: [
           DropdownMenuItem(
             value: "All Namibia",
@@ -123,205 +124,206 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
   Widget build(BuildContext context) {
     double _wd = (MediaQuery.of(context).size.width / 2) - 50;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 16,
-            margin: EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 15,
-              bottom: 15,
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  child: Text(
-                    "Statistics",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: AppColors.primaryText,
-                      fontFamily: "Roboto",
-                      fontWeight: FontWeight.w700,
-                      fontSize: 16,
-                    ),
-                  ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 16,
+                margin: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 15,
+                  bottom: 15,
                 ),
-                Spacer(),
-                Container(
-                  margin: EdgeInsets.only(top: 3),
-                  child: Text(
-                    "As of $refreshDate",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: AppColors.secondaryText,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 13,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ), // Statistics Heading
-          Container(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    StatisticCounter(
-                      width: _wd,
-                      count: 0,
-                      title: 'Confirmed Cases',
-                    ),
-                    StatisticCounter(
-                      width: _wd,
-                      count: 0,
-                      title: 'Confirmed Deaths',
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    StatisticCounter(
-                      width: _wd,
-                      count: 0,
-                      title: 'Recoverd Patients',
-                    ),
-                    StatisticCounter(
-                      width: _wd,
-                      count: 19,
-                      title: 'Suspected Cases',
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ), // Statistics Data
-          Container(
-            height: 16,
-            margin: EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 30,
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    child: Text(
-                      "View By Area",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: AppColors.primaryText,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ), // View By Area Heading
-          Container(
-              height: 50,
-              margin: EdgeInsets.only(
-                left: 20,
-                right: 20,
-                top: 15,
-                bottom: 15,
-              ),
-              decoration: BoxDecoration(
-                color: AppColors.accentElement,
-                borderRadius: BorderRadius.circular(9.0),
-              ),
-              child: Container(
-                  margin: EdgeInsets.only(
-                    left: 15,
-                    right: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    color: AppColors.accentElement,
-                  ),
-                  child: _itemDown())), // Drop Down Menu
-          Divider(),
-          Container(
-            height: 70,
-            margin: EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 15,
-              bottom: 15,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  height: 40,
-                  child: Row(
-                    children: [
-                      Text(
-                        "Push Notifications",
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      child: Text(
+                        "Statistics",
                         textAlign: TextAlign.left,
                         style: TextStyle(
                           color: AppColors.primaryText,
+                          fontFamily: "Roboto",
                           fontWeight: FontWeight.w700,
                           fontSize: 16,
                         ),
                       ),
-                      Spacer(),
-                      Container(
-                        width: 52,
-                        height: 45,
-                        child: Switch(
-                          value: consent,
-                          inactiveTrackColor: Color.fromARGB(60, 0, 0, 0),
-                          onChanged: (value) {
-                            setState(() {
-                              consent = value;
-                              print(consent);
-                            });
-                          },
-                          activeColor: AppColors.primaryElement,
-                          activeTrackColor: AppColors.primaryElement,
+                    ),
+                    Spacer(),
+                    Container(
+                      margin: EdgeInsets.only(top: 3),
+                      child: Text(
+                        "As of $refreshDate",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: AppColors.secondaryText,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 13,
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                Spacer(),
-                Container(
-                  margin: EdgeInsets.only(right: 66),
-                  child: Text(
-                    "By Enabling this option you will receive the latest notifications as they happen.",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: AppColors.secondaryText,
-                      fontSize: 13,
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ), // Push Notifications Consent
-        ],
-      ),
-    );
+              ), // Statistics Heading
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        StatisticCounter(
+                          width: _wd,
+                          count: 0,
+                          title: 'Confirmed Cases',
+                        ),
+                        StatisticCounter(
+                          width: _wd,
+                          count: 0,
+                          title: 'Confirmed Deaths',
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        StatisticCounter(
+                          width: _wd,
+                          count: 0,
+                          title: 'Recoverd Patients',
+                        ),
+                        StatisticCounter(
+                          width: _wd,
+                          count: 19,
+                          title: 'Suspected Cases',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ), // Statistics Data
+              Container(
+                height: 16,
+                margin: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 30,
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        child: Text(
+                          "View By Area",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: AppColors.primaryText,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ), // View By Area Heading
+              Container(
+                  height: 50,
+                  margin: EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    top: 15,
+                    bottom: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColors.accentElement,
+                    borderRadius: BorderRadius.circular(9.0),
+                  ),
+                  child: Container(
+                      margin: EdgeInsets.only(
+                        left: 15,
+                        right: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColors.accentElement,
+                      ),
+                      child: _locationData())), // Drop Down Menu
+              Divider(),
+              Container(
+                height: 70,
+                margin: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 15,
+                  bottom: 15,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 40,
+                      child: Row(
+                        children: [
+                          Text(
+                            "Push Notifications",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: AppColors.primaryText,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Spacer(),
+                          Container(
+                            width: 52,
+                            height: 45,
+                            child: Switch(
+                              value: consent,
+                              inactiveTrackColor: Color.fromARGB(60, 0, 0, 0),
+                              onChanged: (value) {
+                                setState(() {
+                                  consent = value;
+                                  print(consent);
+                                });
+                              },
+                              activeColor: AppColors.primaryElement,
+                              activeTrackColor: AppColors.primaryElement,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      margin: EdgeInsets.only(right: 66),
+                      child: Text(
+                        "By Enabling this option you will receive the latest notifications as they happen.",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: AppColors.secondaryText,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ), // Push Notifications Consent
+            ],
+          ),
+        ));
   }
 }
