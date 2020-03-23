cdf1abbf9df76a060a71e6852d3298847fe8f75c
Herman C. Kandjimi <hkandjimi@nust.na>
2020-03-23 07:54:00 +0200

    google maps on test centres

--- src/Mobile/codebase/flutter/ios/Runner/AppDelegate.swift
+++ src/Mobile/codebase/flutter/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBf3tEmdR18S-2v0E1i1x0oqcXJ9imntT4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
