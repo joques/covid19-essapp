6f777f9b33459574f136b7d6fb9edd3f1394f111
Herman C. Kandjimi <hkandjimi@nust.na>
2020-03-24 15:35:54 +0200

    FAQ sample page

--- src/Mobile/codebase/flutter/lib/widgets/screens/faq_screen.dart
+++ src/Mobile/codebase/flutter/lib/widgets/screens/faq_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:covid_19_app/styles/colors.dart';
 
 class FAQScreen extends StatefulWidget {
   final String title;
@@ -8,19 +9,74 @@ class FAQScreen extends StatefulWidget {
   _FAQScreenState createState() => _FAQScreenState();
 }
 
+class FAQ {
+  FAQ({
+    this.question,
+    this.answer,
+    this.isExpanded = false,
+  });
+
+  String question;
+  String answer;
+  bool isExpanded;
+}
+
+List<FAQ> _data = generateItems(9);
+
+List<FAQ> generateItems(int numberOfItems) {
+  return List.generate(numberOfItems, (int index) {
+    return FAQ(
+      question: 'Question  $index ?',
+      answer:
+          'This is the answer for $index \nLorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean neque massa, rhoncus non quam mattis.',
+    );
+  });
+}
+
 class _FAQScreenState extends State<FAQScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title,
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(widget.title),
-      ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: _buildPanel(),
+          ),
+        ));
+  }
+
+  Widget _buildPanel() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((FAQ item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              leading: Icon(Icons.question_answer),
+              title: Text(item.question,
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                      color: AppColors.primaryText),
+                  textAlign: TextAlign.start),
+            );
+          },
+          body: ListTile(
+            leading: Icon(Icons.lightbulb_outline),
+            title: Text(item.answer),
+          ),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
     );
   }
 }
