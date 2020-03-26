725bf05fb993633e21f5ee063b3e066abce1dadf
Daniel Mgawi <pdmgawi@gmail.com>
2020-03-26 04:25:38 +0200

    1. News Tab, with News UI 2. Circulars & Memos Pending (Need to Design them). 3. Page Routing to symptoms page. 4. Gesture Detector on News Article.

--- src/Mobile/codebase/flutter/lib/widgets/common/news_article.dart
+++ src/Mobile/codebase/flutter/lib/widgets/common/news_article.dart
@@ -8,8 +8,10 @@ class NewsArticle extends StatefulWidget {
   final String title;
   final String author;
   final String desc;
+  final String route;
 
-  NewsArticle({Key key, this.author, this.desc, this.title}) : super(key: key);
+  NewsArticle({Key key, this.author, this.desc, this.title, this.route})
+      : super(key: key);
 
   @override
   _NewsArticleState createState() => _NewsArticleState();
@@ -18,76 +20,81 @@ class NewsArticle extends StatefulWidget {
 class _NewsArticleState extends State<NewsArticle> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(25),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(bottom: 9),
-            height: 46,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: AutoSizeText(
-                    widget.title,
-                    textAlign: TextAlign.left,
-                    style: GoogleFonts.lora(
-                      color: AppColors.primaryText,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 15,
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 9),
+              height: 46,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: AutoSizeText(
+                      widget.title,
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.lora(
+                        color: AppColors.primaryText,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 15,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(bottom: 5),
-            height: 20,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: AutoSizeText(
-                    widget.author,
-                    maxLines: 1,
-                    textAlign: TextAlign.left,
-                    style: GoogleFonts.roboto(
-                      color: AppColors.secondaryText,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14,
+            Container(
+              margin: EdgeInsets.only(bottom: 5),
+              height: 20,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: AutoSizeText(
+                      widget.author,
+                      maxLines: 1,
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.roboto(
+                        color: AppColors.secondaryText,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Container(
-            height: 100,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: AutoSizeText(
-                    widget.desc,
-                    textAlign: TextAlign.left,
-                    minFontSize: 16,
-                    maxLines: 4,
-                    overflow: TextOverflow.fade,
-                    style: GoogleFonts.roboto(
-                      color: AppColors.primaryText,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 17,
+            Container(
+              height: 100,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: AutoSizeText(
+                      widget.desc,
+                      textAlign: TextAlign.left,
+                      minFontSize: 16,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: GoogleFonts.roboto(
+                        color: AppColors.primaryText,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 17,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+      onTap: () {
+        print(widget.title);
+      },
     );
   }
 }
--- src/Mobile/codebase/flutter/lib/widgets/screens/information_screen.dart
+++ src/Mobile/codebase/flutter/lib/widgets/screens/information_screen.dart
@@ -9,7 +9,7 @@ import 'package:http/http.dart' as http;
 
 class InformationScreen extends StatefulWidget {
   final String title;
-  
+
   InformationScreen({Key key, this.title}) : super(key: key);
 
   @override
@@ -20,32 +20,40 @@ class _InformationScreenState extends State<InformationScreen> {
   List _slides = <Widget>[];
   Future<Latest> latestInfo;
   int confirmed;
-  
-  var title = {'1':'What is COVID-19?',
-               '2':"Who is most at risk?",
-               '3':"How is it transmited?",
-               '4':"How can it be treated or cured?",
-               '5':"What are severe complications from this virus? "};
-              
-  var summary1 = {'1':'Coronavirus disease 2019 (COVID-19) is a respiratory illness that can spread from person to person. The virus that causes COVID-19 is a novel coronavirus that was first identified during an investigation into an outbreak in Wuhan, China.',
-               '2':"Currently, travellers to areas where there is ongoing sustained transmission of COVID-19 including Mainland China (all provinces), Hong Kong, Japan, Republic of Korea, Singapore, Vietnam, Taiwan, Italy and the Islamic Republic of Iran are at greatest risk of infection.Furthermore, the elderly, individuals with co-morbidities and healthcare workers have been found to be at a higher risk of death.",
-               '3':"People can catch COVID-19 from others who have the virus. The disease can spread from person to person through small droplets from the nose or mouth which are spread when a person with COVID-19 coughs or exhales. These droplets land on objects and surfaces around the person. Other people then catch COVID-19 by touching these objects or surfaces, then touching their eyes, nose or mouth. People can also catch COVID-19 if they breathe in droplets from a person with COVID-19 who coughs out or exhales droplets. This is why it is important to stay more than 1 meter (3 feet) away from a person who is sick.",
-               '4':"There is currently no vaccine nor any specific antiviral treatment against COVID-19. The best way to prevent infection is to take everyday preventive actions, like avoiding close contact with people who are sick and washing your hands often. ",
-               '5':"Some patients have pneumonia in both lungs, multi-organ failure and in some cases death. "};
-  
+
+  var title = {
+    '1': 'What is COVID-19?',
+    '2': "Who is most at risk?",
+    '3': "How is it transmited?",
+    '4': "How can it be treated or cured?",
+    '5': "What are severe complications from this virus? "
+  };
+
+  var summary1 = {
+    '1':
+        'Coronavirus disease 2019 (COVID-19) is a respiratory illness that can spread from person to person. The virus that causes COVID-19 is a novel coronavirus that was first identified during an investigation into an outbreak in Wuhan, China.',
+    '2':
+        "Currently, travellers to areas where there is ongoing sustained transmission of COVID-19 including Mainland China (all provinces), Hong Kong, Japan, Republic of Korea, Singapore, Vietnam, Taiwan, Italy and the Islamic Republic of Iran are at greatest risk of infection.Furthermore, the elderly, individuals with co-morbidities and healthcare workers have been found to be at a higher risk of death.",
+    '3':
+        "People can catch COVID-19 from others who have the virus. The disease can spread from person to person through small droplets from the nose or mouth which are spread when a person with COVID-19 coughs or exhales. These droplets land on objects and surfaces around the person. Other people then catch COVID-19 by touching these objects or surfaces, then touching their eyes, nose or mouth. People can also catch COVID-19 if they breathe in droplets from a person with COVID-19 who coughs out or exhales droplets. This is why it is important to stay more than 1 meter (3 feet) away from a person who is sick.",
+    '4':
+        "There is currently no vaccine nor any specific antiviral treatment against COVID-19. The best way to prevent infection is to take everyday preventive actions, like avoiding close contact with people who are sick and washing your hands often. ",
+    '5':
+        "Some patients have pneumonia in both lungs, multi-organ failure and in some cases death. "
+  };
+
   @override
   Widget build(BuildContext context) {
-    
     @override
-    void initState(){
+    void initState() {
       super.initState();
       this.latestInfo = fetchInfo();
-      latestInfo.then((value) =>{
-        this.confirmed =int.parse(value.confirmed.toString())
-      });
-      debugPrint("CONFIRMED=>"+this.confirmed.toString());
+      latestInfo.then(
+          (value) => {this.confirmed = int.parse(value.confirmed.toString())});
+      debugPrint("CONFIRMED=>" + this.confirmed.toString());
       // Additional initialization of the State
     }
+
     double _wd = (MediaQuery.of(context).size.width / 2) - 50;
 
     return Scaffold(
@@ -55,14 +63,13 @@ class _InformationScreenState extends State<InformationScreen> {
           ),
           centerTitle: true,
         ),
-        body: 
-          SingleChildScrollView(
+        body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
               children: <Widget>[
                 CarouselSlider(
-                  height:200,
+                  height: 200,
                   viewportFraction: 1.0,
                   aspectRatio: 2.0,
                   autoPlay: true,
@@ -100,7 +107,6 @@ class _InformationScreenState extends State<InformationScreen> {
                                           color: AppColors.primaryText,
                                           fontSize: 16),
                                 )
-                            
                               ],
                             ),
                           ),
@@ -121,11 +127,15 @@ class _InformationScreenState extends State<InformationScreen> {
                       shape: BeveledRectangleBorder(
                           borderRadius: BorderRadius.only(
                               bottomRight: Radius.circular(16))),
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/symptoms');
+                      },
                       child: Text('Symptoms'.toUpperCase()),
                     ),
                     RaisedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/symptoms');
+                      },
                       color: AppColors.ternaryBackground,
                       shape: BeveledRectangleBorder(
                           borderRadius: BorderRadius.only(
@@ -158,7 +168,6 @@ class _InformationScreenState extends State<InformationScreen> {
                   ],
                 ),
                 Container(
-                  
                   child: Column(
                     children: <Widget>[
                       SizedBox(
@@ -170,13 +179,13 @@ class _InformationScreenState extends State<InformationScreen> {
                           StatisticCounter(
                             width: _wd,
                             count: 4,
-                            borderColor:Colors.blue.shade800.value,
+                            borderColor: Colors.blue.shade800.value,
                             title: 'Confirmed Cases',
                           ),
                           StatisticCounter(
                             width: _wd,
                             count: 0,
-                            borderColor:Colors.red.shade900.value,
+                            borderColor: Colors.red.shade900.value,
                             title: 'Confirmed Deaths',
                           ),
                         ],
@@ -190,13 +199,13 @@ class _InformationScreenState extends State<InformationScreen> {
                           StatisticCounter(
                             width: _wd,
                             count: 4,
-                            borderColor:Colors.green.shade900.value,
+                            borderColor: Colors.green.shade900.value,
                             title: 'Recoverd Patients',
                           ),
                           StatisticCounter(
                             width: _wd,
                             count: 19,
-                            borderColor:Colors.orange.shade900.value,
+                            borderColor: Colors.orange.shade900.value,
                             title: 'Suspected Cases',
                           ),
                         ],
@@ -212,19 +221,21 @@ class _InformationScreenState extends State<InformationScreen> {
         );
   }
 }
+
 Future<Latest> fetchInfo() async {
   //http://196.216.167.150:6549/covid/v1/statistics/latest');
-  final response = await http.get("http://196.216.167.150:6549/covid/v1/statistics/latest");
+  final response =
+      await http.get("http://196.216.167.150:6549/covid/v1/statistics/latest");
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    debugPrint("Data=>"+response.body.toString());
+    debugPrint("Data=>" + response.body.toString());
     return Latest.fromJson(json.decode(response.body));
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-    debugPrint("HERE"+response.toString());
+    debugPrint("HERE" + response.toString());
     throw Exception('Failed to load latest info');
   }
 }
