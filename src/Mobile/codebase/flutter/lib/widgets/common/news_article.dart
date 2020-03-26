852cbabce18acbd219e60b892cb873f435f9284f
Daniel Mgawi <pdmgawi@gmail.com>
2020-03-26 17:09:05 +0200

    News Tab
    News Widgets working, Memo and Circulars yet to be designed.

--- src/Mobile/codebase/flutter/lib/widgets/common/news_article.dart
+++ src/Mobile/codebase/flutter/lib/widgets/common/news_article.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:covid_19_app/styles/colors.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shimmer/shimmer.dart';
 
 class NewsArticle extends StatefulWidget {
   final String title;
   final String author;
   final String desc;
+  final String imageSrc;
   final String route;
 
-  NewsArticle({Key key, this.author, this.desc, this.title, this.route})
+  NewsArticle(
+      {Key key,
+      @required this.author,
+      @required this.imageSrc,
+      @required this.desc,
+      @required this.title,
+      this.route})
       : super(key: key);
 
   @override
@@ -22,62 +29,79 @@ class _NewsArticleState extends State<NewsArticle> {
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Container(
-        margin: EdgeInsets.all(25),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(bottom: 9),
-              height: 46,
-              child: Row(
+        height: 150,
+        margin: EdgeInsets.only(
+          left: 23,
+          top: 20,
+          right: 17,
+          bottom: 10,
+        ),
+        child: Stack(
+          alignment: Alignment.topRight,
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                decoration: BoxDecoration(
+//                  border: Border.all(
+//                    width: 1,
+//                    color: Color.fromARGB(255, 112, 112, 112),
+//                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                ),
+                width: 95,
+                height: 140,
+                child: Image.network(
+                  widget.imageSrc,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 10,
+              right: 11,
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
+                  Container(
+                    margin: EdgeInsets.only(left: 103, bottom: 9),
                     child: AutoSizeText(
                       widget.title,
                       textAlign: TextAlign.left,
+                      maxLines: 2,
                       style: GoogleFonts.lora(
                         color: AppColors.primaryText,
+                        letterSpacing: 2.0,
                         fontWeight: FontWeight.w700,
-                        fontSize: 15,
+                        fontSize: 16,
                       ),
                     ),
                   ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 5),
-              height: 20,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    child: AutoSizeText(
-                      widget.author,
-                      maxLines: 1,
-                      textAlign: TextAlign.left,
-                      style: GoogleFonts.roboto(
-                        color: AppColors.secondaryText,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14,
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      margin: EdgeInsets.only(left: 103, bottom: 4),
+                      child: AutoSizeText(
+                        widget.author,
+                        maxLines: 1,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 112, 112, 112),
+                          fontFamily: "Roboto",
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
                       ),
                     ),
                   ),
-                ],
-              ),
-            ),
-            Container(
-              height: 100,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
+                  Container(
+                    height: 85,
+                    margin: EdgeInsets.only(left: 103, top: 8),
                     child: AutoSizeText(
                       widget.desc,
                       textAlign: TextAlign.left,
-                      minFontSize: 16,
-                      maxLines: 3,
+                      maxLines: 4,
+                      minFontSize: 14,
                       overflow: TextOverflow.ellipsis,
                       style: GoogleFonts.roboto(
                         color: AppColors.primaryText,
--- src/Mobile/codebase/flutter/lib/widgets/screens/news_screen.dart
+++ src/Mobile/codebase/flutter/lib/widgets/screens/news_screen.dart
@@ -1,5 +1,6 @@
 import 'package:covid_19_app/styles/colors.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:covid_19_app/widgets/common/featured_article.dart';
 import 'package:covid_19_app/widgets/common/news_article.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -50,84 +51,13 @@ class _NewsScreenState extends State<NewsScreen> {
               ],
             ),
           ), // TODO: Featured News Widget
-          Container(
-            margin: EdgeInsets.all(28),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(bottom: 9),
-                  height: 50,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: AutoSizeText(
-                          "Title that is very long and informative about the topic",
-                          maxLines: 2,
-                          textAlign: TextAlign.left,
-                          style: GoogleFonts.lora(
-                            color: AppColors.primaryText,
-                            letterSpacing: 2.0,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 19,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 4),
-                  height: 20,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: AutoSizeText(
-                          "Author Name",
-                          maxLines: 1,
-                          textAlign: TextAlign.left,
-                          style: GoogleFonts.roboto(
-                            color: AppColors.secondaryText,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 140,
-                  decoration: BoxDecoration(
-                    color: AppColors.secondaryBackground,
-                    borderRadius: BorderRadius.circular(9.0),
-                  ),
-                  child: Container(), //TODO: Image Goes Here
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 9.0),
-                  height: 100,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: AutoSizeText(
-                          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat.",
-                          textAlign: TextAlign.left,
-                          minFontSize: 14,
-                          style: GoogleFonts.roboto(
-                            color: AppColors.primaryText,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 17,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+          FeaturedArticle(
+            author: 'The World Health Organisation',
+            title: 'Coronavirus disease (COVID-19) Pandemic',
+            desc:
+                'The disease causes respiratory illness (like the flu) with symptoms such as a cough, fever, and in more severe cases, difficulty breathing. You can protect yourself by washing your hands frequently, avoiding touching your face, and avoiding close contact (1 meter or 3 feet) with people who are unwell.',
+            imageSrc:
+                'https://images.pexels.com/photos/415779/pexels-photo-415779.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
           ), // Featured Post With AutoSized Text
           Container(
             height: 25,
@@ -156,14 +86,18 @@ class _NewsScreenState extends State<NewsScreen> {
           ), // Latest News
           Divider(),
           NewsArticle(
-            title: 'Covid-19 cases',
-            author: 'Gov Ministry',
-            desc:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat.",
-          ),
+              title: 'Covid-19 war command prepares for virus battle',
+              author: 'NGO Name',
+              imageSrc:
+                  'https://images.pexels.com/photos/3843955/pexels-photo-3843955.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+              desc:
+                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat."),
+
           NewsArticle(
               title: 'Covid-19 war command prepares for virus battle',
               author: 'NGO Name',
+              imageSrc:
+                  'https://images.pexels.com/photos/3843955/pexels-photo-3843955.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
               desc:
                   "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat."),
         ],
