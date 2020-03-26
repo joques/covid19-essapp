9ef897116c8c0068c462b6510db680dbb1a95935
Naftal Timotheus <tnaffh@gmail.com>
2020-03-26 08:24:25 +0200

    first page fixe

--- src/Mobile/codebase/flutter/lib/widgets/screens/information_screen.dart
+++ src/Mobile/codebase/flutter/lib/widgets/screens/information_screen.dart
@@ -4,12 +4,13 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:covid_19_app/styles/colors.dart';
 import 'package:covid_19_app/widgets/common/statistic_counter.dart';
 import 'package:covid_19_app/widgets/common/statistical_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class InformationScreen extends StatefulWidget {
   final String title;
-  
+
   InformationScreen({Key key, this.title}) : super(key: key);
 
   @override
@@ -20,32 +21,40 @@ class _InformationScreenState extends State<InformationScreen> {
   List _slides = <Widget>[];
   Future<Latest> latestInfo;
   int confirmed;
-  
-  var title = {'1':'What is COVID-19?',
-               '2':"Who is most at risk?",
-               '3':"How is it transmited?",
-               '4':"How can it be treated or cured?",
-               '5':"What are severe complications from this virus? "};
-              
-  var summary1 = {'1':'Coronavirus disease 2019 (COVID-19) is a respiratory illness that can spread from person to person. The virus that causes COVID-19 is a novel coronavirus that was first identified during an investigation into an outbreak in Wuhan, China.',
-               '2':"Currently, travellers to areas where there is ongoing sustained transmission of COVID-19 including Mainland China (all provinces), Hong Kong, Japan, Republic of Korea, Singapore, Vietnam, Taiwan, Italy and the Islamic Republic of Iran are at greatest risk of infection.Furthermore, the elderly, individuals with co-morbidities and healthcare workers have been found to be at a higher risk of death.",
-               '3':"People can catch COVID-19 from others who have the virus. The disease can spread from person to person through small droplets from the nose or mouth which are spread when a person with COVID-19 coughs or exhales. These droplets land on objects and surfaces around the person. Other people then catch COVID-19 by touching these objects or surfaces, then touching their eyes, nose or mouth. People can also catch COVID-19 if they breathe in droplets from a person with COVID-19 who coughs out or exhales droplets. This is why it is important to stay more than 1 meter (3 feet) away from a person who is sick.",
-               '4':"There is currently no vaccine nor any specific antiviral treatment against COVID-19. The best way to prevent infection is to take everyday preventive actions, like avoiding close contact with people who are sick and washing your hands often. ",
-               '5':"Some patients have pneumonia in both lungs, multi-organ failure and in some cases death. "};
-  
+
+  var title = {
+    '1': 'What is COVID-19?',
+    '2': "Who is most at risk?",
+    '3': "How is it transmited?",
+    '4': "How can it be treated or cured?",
+    '5': "What are severe complications from this virus? "
+  };
+
+  var summary1 = {
+    '1':
+        'Coronavirus disease 2019 (COVID-19) is a respiratory illness that can spread from person to person. The virus that causes COVID-19 is a novel coronavirus that was first identified during an investigation into an outbreak in Wuhan, China.',
+    '2':
+        "Currently, travellers to areas where there is ongoing sustained transmission of COVID-19 including Mainland China (all provinces), Hong Kong, Japan, Republic of Korea, Singapore, Vietnam, Taiwan, Italy and the Islamic Republic of Iran are at greatest risk of infection.Furthermore, the elderly, individuals with co-morbidities and healthcare workers have been found to be at a higher risk of death.",
+    '3':
+        "People can catch COVID-19 from others who have the virus. The disease can spread from person to person through small droplets from the nose or mouth which are spread when a person with COVID-19 coughs or exhales. These droplets land on objects and surfaces around the person. Other people then catch COVID-19 by touching these objects or surfaces, then touching their eyes, nose or mouth. People can also catch COVID-19 if they breathe in droplets from a person with COVID-19 who coughs out or exhales droplets. This is why it is important to stay more than 1 meter (3 feet) away from a person who is sick.",
+    '4':
+        "There is currently no vaccine nor any specific antiviral treatment against COVID-19. The best way to prevent infection is to take everyday preventive actions, like avoiding close contact with people who are sick and washing your hands often. ",
+    '5':
+        "Some patients have pneumonia in both lungs, multi-organ failure and in some cases death. "
+  };
+
   @override
   Widget build(BuildContext context) {
-    
     @override
-    void initState(){
+    void initState() {
       super.initState();
       this.latestInfo = fetchInfo();
-      latestInfo.then((value) =>{
-        this.confirmed =int.parse(value.confirmed.toString())
-      });
-      debugPrint("CONFIRMED=>"+this.confirmed.toString());
+      latestInfo.then(
+          (value) => {this.confirmed = int.parse(value.confirmed.toString())});
+      debugPrint("CONFIRMED=>" + this.confirmed.toString());
       // Additional initialization of the State
     }
+
     double _wd = (MediaQuery.of(context).size.width / 2) - 50;
 
     return Scaffold(
@@ -55,14 +64,13 @@ class _InformationScreenState extends State<InformationScreen> {
           ),
           centerTitle: true,
         ),
-        body: 
-          SingleChildScrollView(
+        body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Column(
               children: <Widget>[
                 CarouselSlider(
-                  height:200,
+                  height: 200,
                   viewportFraction: 1.0,
                   aspectRatio: 2.0,
                   autoPlay: true,
@@ -71,6 +79,7 @@ class _InformationScreenState extends State<InformationScreen> {
                     return Builder(
                       builder: (BuildContext context) {
                         return Card(
+                          color: Theme.of(context).primaryColor,
                           elevation: 5,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(15.0)),
@@ -82,10 +91,12 @@ class _InformationScreenState extends State<InformationScreen> {
                               children: <Widget>[
                                 Text(
                                   title['$i'],
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
                                   style: TextStyle(
-                                      fontSize: 25,
+                                      fontSize: 18,
                                       fontWeight: FontWeight.w900,
-                                      color: AppColors.secondaryText),
+                                      color: Colors.white),
                                   textAlign: TextAlign.start,
                                 ),
                                 SizedBox(
@@ -93,14 +104,14 @@ class _InformationScreenState extends State<InformationScreen> {
                                 ),
                                 Text(
                                   summary1['$i'],
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 5,
                                   style: Theme.of(context)
                                       .textTheme
-                                      .body2
+                                      .bodyText2
                                       .copyWith(
-                                          color: AppColors.primaryText,
-                                          fontSize: 16),
+                                          color: Colors.white, fontSize: 16),
                                 )
-                            
                               ],
                             ),
                           ),
@@ -158,7 +169,6 @@ class _InformationScreenState extends State<InformationScreen> {
                   ],
                 ),
                 Container(
-                  
                   child: Column(
                     children: <Widget>[
                       SizedBox(
@@ -170,13 +180,13 @@ class _InformationScreenState extends State<InformationScreen> {
                           StatisticCounter(
                             width: _wd,
                             count: 4,
-                            borderColor:Colors.blue.shade800.value,
+                            borderColor: Colors.blue.shade800.value,
                             title: 'Confirmed Cases',
                           ),
                           StatisticCounter(
                             width: _wd,
                             count: 0,
-                            borderColor:Colors.red.shade900.value,
+                            borderColor: Colors.red.shade900.value,
                             title: 'Confirmed Deaths',
                           ),
                         ],
@@ -190,13 +200,13 @@ class _InformationScreenState extends State<InformationScreen> {
                           StatisticCounter(
                             width: _wd,
                             count: 4,
-                            borderColor:Colors.green.shade900.value,
+                            borderColor: Colors.green.shade900.value,
                             title: 'Recoverd Patients',
                           ),
                           StatisticCounter(
                             width: _wd,
                             count: 19,
-                            borderColor:Colors.orange.shade900.value,
+                            borderColor: Colors.orange.shade900.value,
                             title: 'Suspected Cases',
                           ),
                         ],
@@ -212,19 +222,21 @@ class _InformationScreenState extends State<InformationScreen> {
         );
   }
 }
+
 Future<Latest> fetchInfo() async {
   //http://196.216.167.150:6549/covid/v1/statistics/latest');
-  final response = await http.get("http://196.216.167.150:6549/covid/v1/statistics/latest");
+  final response =
+      await http.get("http://196.216.167.150:6549/covid/v1/statistics/latest");
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    debugPrint("Data=>"+response.body.toString());
+    debugPrint("Data=>" + response.body.toString());
     return Latest.fromJson(json.decode(response.body));
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-    debugPrint("HERE"+response.toString());
+    debugPrint("HERE" + response.toString());
     throw Exception('Failed to load latest info');
   }
 }
