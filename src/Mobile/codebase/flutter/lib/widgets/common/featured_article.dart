23b201920e0811818678b722a8c8e1b407f5c673
Daniel Mgawi <pdmgawi@gmail.com>
2020-03-28 14:30:28 +0200

    1. Combined Tabs on News Tab. 2. PDF Rendering in App, //TODO: Download PDF and View PDF Option.

--- src/Mobile/codebase/flutter/lib/widgets/common/featured_article.dart
+++ src/Mobile/codebase/flutter/lib/widgets/common/featured_article.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:covid_19_app/styles/colors.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shimmer/shimmer.dart';
 
 class FeaturedArticle extends StatefulWidget {
   final String title;
