beadb747a648384df52cc165ffb2d864e11c6ca1
Daniel Mgawi <pdmgawi@gmail.com>
2020-04-06 07:35:22 +0200

    Logo's Changed
    Sponsor page and Launch Screen

--- src/Mobile/codebase/flutter/lib/widgets/screens/launch_screen.dart
+++ src/Mobile/codebase/flutter/lib/widgets/screens/launch_screen.dart
@@ -54,70 +54,77 @@ class SplashScreenState extends State<SplashScreen> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Wrap(
-          crossAxisAlignment: WrapCrossAlignment.center,
-          alignment: WrapAlignment.center,
-          runSpacing: 30,
-          runAlignment: WrapAlignment.spaceEvenly,
-          spacing: 50,
-          children: <Widget>[
-            Container(
-              width: 200,
-              child: Image.asset(
-                'assets/images/logo.jpeg',
-                fit: BoxFit.contain,
-              ),
-            ),
-            spinKit,
-            Text(
-              "Proudly brought to you by:",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: AppColors.primaryText,
-                fontFamily: "Roboto",
-                fontWeight: FontWeight.w800,
-                fontSize: 13,
-              ),
-            ),
-            Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              alignment: WrapAlignment.spaceBetween,
-              spacing: 35,
-              runSpacing: 50,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/sponsors/NUSTCoBrandLogo.png',
-                  height: 45,
-                ),
-                Image.asset(
-                  'assets/images/sponsors/liberity.png',
-                  height: 70,
-                ),
-              ],
-            ),
-            RaisedButton.icon(
-              onPressed: () => UrlLauncher.launch(
-                  '$tollFreeNumber'), //TODO: import urlLauncher to Make a call
-              color: AppColors.primaryElement,
-              shape: BeveledRectangleBorder(
-                  borderRadius:
-                      BorderRadius.only(bottomRight: Radius.circular(16))),
-              label: Text(
-                'Call Help Line'.toUpperCase(),
-                style: TextStyle(color: AppColors.accentElement),
-              ),
-              icon: Padding(
-                padding: EdgeInsets.all(
-                  4.0,
-                ),
-                child: Icon(
-                  LineIcons.phone,
-                  color: AppColors.accentElement,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            alignment: WrapAlignment.center,
+            runSpacing: 30,
+            runAlignment: WrapAlignment.spaceEvenly,
+            spacing: 50,
+            children: <Widget>[
+              Container(
+                width: 200,
+                child: Image.asset(
+                  'assets/images/logo.jpeg',
+                  fit: BoxFit.contain,
                 ),
               ),
-            ),
-          ],
+              spinKit,
+              Text(
+                "Proudly brought to you by:",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.primaryText,
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.w800,
+                  fontSize: 13,
+                ),
+              ),
+              Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                alignment: WrapAlignment.spaceBetween,
+                spacing: 35,
+                runSpacing: 50,
+                children: <Widget>[
+                  SvgPicture.asset(
+                    'assets/images/sponsors/NUST&MOHSS.svg',
+                    height: 62,
+                  ),
+                  // Image.asset(
+                  //   'assets/images/sponsors/NUSTCoBrandLogo.png',
+                  //   height: 45,
+                  // ),
+                  // Image.asset(
+                  //   'assets/images/sponsors/liberity.png',
+                  //   height: 70,
+                  // ),
+                ],
+              ),
+              RaisedButton.icon(
+                onPressed: () => UrlLauncher.launch(
+                    '$tollFreeNumber'), //TODO: import urlLauncher to Make a call
+                color: AppColors.primaryElement,
+                shape: BeveledRectangleBorder(
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(16))),
+                label: Text(
+                  'Call Help Line'.toUpperCase(),
+                  style: TextStyle(color: AppColors.accentElement),
+                ),
+                icon: Padding(
+                  padding: EdgeInsets.all(
+                    4.0,
+                  ),
+                  child: Icon(
+                    LineIcons.phone,
+                    color: AppColors.accentElement,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
